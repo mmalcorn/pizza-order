80330cf7a74fd578b4ee5aebeec9b794fe5b825d
Meredith Alcorn <alcornmeredith@gmail.com>
2016-08-28 14:10:37 -0700

    add business logic by creating a branching statement for when ping, pong, and pingpong should appear on the screen

--- Desktop/ping-pong/js/scripts.js
+++ Desktop/ping-pong/js/scripts.js
@@ -3,12 +3,26 @@ $(document).ready(function(){
   event.preventDefault();
   var userNumber = $("#userNumber").val();
   var numberArray = [];
+  var start = 0;
+
+  numberArray.push($("#userNumber").val());
+  if(numberArray % 15 === 0){
+    numberArray.push("pingpong")
   }
+  else if(numberArray % 5 === 0){
+    numberArray.push("ping")
+  }
+  else if(numberArray % 3 === 0){
+    numberArray.push("pong")
+  }
+  (console.log(numberArray))
 
 
 
 
 
+
+//TODO: Numbers need to show in an unordered list.  Right now they display right next to each other.
 $("#show-numbers").append(numberArray);
 
 
