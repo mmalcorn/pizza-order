2b682f9ca4a28959caa359ec1027b66724ff9845
Meredith Alcorn <alcornmeredith@gmail.com>
2016-08-29 21:37:36 -0700

    add a for loop

--- Desktop/ping-pong/js/scripts.js
+++ Desktop/ping-pong/js/scripts.js
@@ -2,27 +2,37 @@ $(document).ready(function(){
   $("#ping-pong").submit(function(){
   event.preventDefault();
   var numberArray = [];
-  var userNumber = $("#userNumber").val();
+  var pingPong = [];
+  // var userNumber = $(parseInt("#userNumber")).val();
   numberArray.push($("#userNumber").val());
   $("ul").append("<br>" + numberArray +"</br>");
 
-
   if (numberArray < 1 ){
     alert("Enter a number above 0")
-  }else{
-    //Display all numbers up to the number the user enters with a loop
-}
   }
 
- if(numberArray % 15 === 0){
-        numberArray.push("pingpong");
-      }
-      else if(numberArray % 5 === 0){
-        numberArray.push("ping")
-      }
-      else if(numberArray % 3 === 0){
-          numberArray.push("pong")
-      }
-})
 
+  for (var index = 1 ; index <= numberArray; index++){
+
+  }
+
+  if(numberArray % 15 === 0){
+         pingPong.push("pingpong")
+       }
+       else if(numberArray % 5 === 0){
+         pingPong.push("ping")
+       }
+       else if(numberArray % 3 === 0){
+           pingPong.push("pong")
+           }
+       else{
+         pingPong.push(index);
+  // var concatArrays = numberArray.concat(pingPong);
+
+
+  }
+      //  (console.log(numberArray));
+      //  (console.log(pingPong))
+
+  })
 });
